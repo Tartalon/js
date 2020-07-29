ea5acec0abca9f70277cb4bd8afa7485c870dbb1
tartalon <tartalon23@gmail.com>
2020-07-30 00:01:32 +0300

    function guessTheNumber done

--- function/task/script.js
+++ function/task/script.js
@@ -16,8 +16,6 @@
 // }
 // console.log(multiply(2, 4, 5, 6));
 
-
-
 // Создать функцию, которая принимает строку и возвращает строку-перевертыш:
 // reverseString('test'); // 'tset'
 // reverseString(''); // ''
@@ -34,8 +32,6 @@
 //   return revStr;
 // }
 
-
-
 // Создайте ф-ю, которая в качестве аргумента может принять строку, числа, null или undefined и возвращает строку, где каждый символ расделен пробелом и заменен на юникод-значение символа:
 // getCodeStringFromText("hello") // “104 101 108 108 111”
 // подсказка: в решении задачи вам помогут методы charCodeAt и trim
@@ -50,29 +46,32 @@
 // }
 // console.log(reverseString('hello'));
 
-
-
-
 // Создать функцию угадай число. Она принимает число от 1-10 (обязательно проверить что число не больше 10 и не меньше 0). Если число не соответствует условию то верните ошибку return new Error("Please provide number in range 0 - 10"). Если переданно не число то верните ошибку return  new Error("Please provide a valid number");
 // Далле функция генерирует рандомное число от 1-10 и сравнивает с заданным числом если они совпали то возвращает строку "You win!". Eсли нет то строку "You are lose, your number is 8, the random number is 5". Числа в строке указаны как пример вы подставляете реальные числа.
 // Если переданно число в виде строки оно должно быть преобразованно к числу.
 
 function guessTheNumber(num) {
   num = +num;
-  let unswer = '';
+  let randomNumber = Math.floor(Math.random() * 10 + 1);
+  let unswer = "";
   if (num <= 10 && num >= 0) {
-    let randomNumber = Math.floor(Math.random() * num) + 1;
-    if(num === randomNumber) {
+    console.log(randomNumber);
+    if (num === randomNumber) {
       unswer = "You win!";
-    } else if (num.typeof != Number) {
-      return  new Error("Please provide a valid number");
+    } else if (typeof num !== "number") {
+      unswer = new Error("Please provide a valid number");
+      console.log(num);
     } else {
-      return new Error("Please provide number in range 0 - 10");
+      unswer =
+        "You are lose, your number is " +
+        num +
+        " the random number is " +
+        randomNumber;
     }
     return unswer;
+  } else {
+    return new Error("Please provide number in range 0 - 10");
   }
-  return randomNumber;
 }
 
-console.log(guessTheNumber(5));
-
+console.log(guessTheNumber("12"));
