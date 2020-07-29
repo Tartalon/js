8083d33c8bb8c7f670a9854c03d00835379a753e
Vladimir <50200804+Tartalon@users.noreply.github.com>
2020-07-29 18:27:47 +0300

    start fuction guessTheNumber

--- function/task/script.js
+++ function/task/script.js
@@ -25,23 +25,54 @@
 // reverseString(undefined); // denifednu
 // reverseString(); // denifednu
 
-function reverseString(str) {
-  let newStr = String(str);
-  let revStr = '';
-  for (let i = newStr.length - 1; i >= 0; i--) {
-    revStr += newStr[i];
-  }
-  console.log(revStr);
-}
-
 // function reverseString(str) {
-//   newStr = str.toString();
-//   return newStr;
+//   let newStr = String(str);
+//   let revStr = '';
+//   for (let i = newStr.length - 1; i >= 0; i--) {
+//     revStr += newStr[i];
+//   }
+//   return revStr;
 // }
 
-// reverseString('test');
-// reverseString('');
-// reverseString(null);
-// reverseString(undefined);
-// reverseString();
-console.log(reverseString(''));
+
+
+// Создайте ф-ю, которая в качестве аргумента может принять строку, числа, null или undefined и возвращает строку, где каждый символ расделен пробелом и заменен на юникод-значение символа:
+// getCodeStringFromText("hello") // “104 101 108 108 111”
+// подсказка: в решении задачи вам помогут методы charCodeAt и trim
+
+// function reverseString(str) {
+//   let newStr = String(str);
+//   let revStr = '';
+//   for (let i = 0; i < newStr.length; i++) {
+//     revStr += newStr.charCodeAt([i]) + ' ';
+//   }
+//   return revStr.trim();
+// }
+// console.log(reverseString('hello'));
+
+
+
+
+// Создать функцию угадай число. Она принимает число от 1-10 (обязательно проверить что число не больше 10 и не меньше 0). Если число не соответствует условию то верните ошибку return new Error("Please provide number in range 0 - 10"). Если переданно не число то верните ошибку return  new Error("Please provide a valid number");
+// Далле функция генерирует рандомное число от 1-10 и сравнивает с заданным числом если они совпали то возвращает строку "You win!". Eсли нет то строку "You are lose, your number is 8, the random number is 5". Числа в строке указаны как пример вы подставляете реальные числа.
+// Если переданно число в виде строки оно должно быть преобразованно к числу.
+
+function guessTheNumber(num) {
+  num = +num;
+  let unswer = '';
+  if (num <= 10 && num >= 0) {
+    let randomNumber = Math.floor(Math.random() * num) + 1;
+    if(num === randomNumber) {
+      unswer = "You win!";
+    } else if (num.typeof != Number) {
+      return  new Error("Please provide a valid number");
+    } else {
+      return new Error("Please provide number in range 0 - 10");
+    }
+    return unswer;
+  }
+  return randomNumber;
+}
+
+console.log(guessTheNumber(5));
+
