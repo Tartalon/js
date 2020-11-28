568570b27a731210855194773b138248bce3e02b
tartalon <tartalon23@gmail.com>
2020-11-28 09:45:44 +0200

    objConstructor added pet

--- MDN/objConstructor/js/script.js
+++ MDN/objConstructor/js/script.js
@@ -1,4 +1,4 @@
-function Person(first, last, age, gender, interests) {
+function Person(first, last, age, gender, interests, pet) {
   this.name = {
     first: first,
     last: last
@@ -6,12 +6,16 @@ function Person(first, last, age, gender, interests) {
   this.age = age;
   this.gender = gender;
   this.interests = interests;
+  this.pet = pet;
   this.bio = function() {
     alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + tis.interests[1] + '.');
   };
   this.greeting = function() {
     alert('Hi! I\'m ' + this.name.first + '.');
   };
+  this.animals = function() {
+    alert(this.name.first + ' have a ' + this.pet);
+  };
 }
 
-let person1 = new Pweson('Bob', 'Smith', '32', 'male', ['music', 'skiing']);
+let person1 = new Person('Bob', 'Smith', '32', 'male', ['music', 'skiing'], 'cat');
