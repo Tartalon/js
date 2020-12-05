31c1fe91d21b907ff3529db09898fae61070c893
tartalon <tartalon23@gmail.com>
2020-12-05 16:03:19 +0200

    criating finished constructor

--- MDN/objConstructor/js/script.js
+++ MDN/objConstructor/js/script.js
@@ -8,7 +8,35 @@ function Person(first, last, age, gender, interests, pet) {
   this.interests = interests;
   this.pet = pet;
   this.bio = function() {
-    alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + tis.interests[1] + '.');
+    let string = this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. ';
+
+    let pronoun;
+
+    if(this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
+      pronoun = 'He likes ';
+    } else if(this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
+      pronoun = 'She likes ';
+    } else {
+      pronoun = 'Thye like ';
+    }
+
+    string += pronoun;
+
+    if(this.interests.length === 1) {
+      string += this.interests[0] + '.';
+    } else if(this.interests.length === 2) {
+      string += this.interests[0] + ' and ' + this.interests[1] + '.';
+    } else {
+      for(let i = 0; i < this.interests.length; i++) {
+        if(i === this.interests.length - 1) {
+          string += 'and ' + this.interests[i] + '.';
+        } else {
+          string += this.interests[i] + ', ';
+        }
+      }
+    }
+
+    alert(string);
   };
   this.greeting = function() {
     alert('Hi! I\'m ' + this.name.first + '.');
