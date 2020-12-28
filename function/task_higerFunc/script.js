4b8a8ef4a1a3a5e7e60828caa944da27e382276c
Vladimir <50200804+Tartalon@users.noreply.github.com>
2020-12-28 18:01:28 +0200

    task func higerFunc start

--- function/task_higerFunc/script.js
+++ function/task_higerFunc/script.js
@@ -12,29 +12,34 @@
 
 // 1
 function firstFunc(arr, fn) {
-
+  let res = '';
+  for (let i = 0; i < arr.length; i++) {
+    res += (fn(arr[i]));
+  }
+  return res;
 }
 
 function handler1(el) {
-
+  let newArr = el.replace(el[0], el[0].toUpperCase());
+  return newArr;
 }
 
 console.log(firstFunc(['my', 'name', 'is', 'Trinity'], handler1));
 
-function handler2(el) {
+// function handler2(el) {
 
-}
+// }
 
-console.log(firstFunc([10, 20, 30], handler2) )
+// console.log(firstFunc([10, 20, 30], handler2) )
 
-function handler3(el) {
+// function handler3(el) {
 
-}
+// }
 
-console.log(firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], handler3));
+// console.log(firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], handler3));
 
-function handler4(el) {
+// function handler4(el) {
 
-}
+// }
 
-console.log(firstFunc(['abs', '123'], handler4));
+// console.log(firstFunc(['abs', '123'], handler4));
