0fcb036c59522579e41ae405877e41f8f3d4efd7
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-06-04 19:20:47 +0300

    task 26 - 1 done

--- 26_obj/js/script.js
+++ 26_obj/js/script.js
@@ -1,3 +1,15 @@
+function getSecondsInTime(s) {
+  let hour = Math.floor(s / 3600) % 24;
+  let sec = s % 60;
+  let min = Math.round(s / 60) % 60;
+
+  if (hour < 10) hour = '0' + hour;
+  if (min < 10) min = '0' + min;
+  if (sec < 10) sec = '0' + sec;
+
+  return `Time is: ${hour}:${min}:${sec}`;
+}
+
 // 1 ================
 // Создать объект, описывающий автомобиль (производитель, модель, год выпуска, средняя скорость), и следующие функции для работы с этим объектом:
 // Функция для вывода на экран информации об автомобиле;
@@ -8,10 +20,18 @@ let car = {
   model: 'Prado',
   year: '2004',
   speed: '80',
+
+  getCarInfo: function () {
+    return `The car brand: ${this.brand}, model: ${this.model}, year: ${this.year}, speed: ${this.speed}`;
+  },
+
+  travelTime: function (distance) {
+    let timeInSeconds = (distance / this.speed) * 3600;
+    let rest = Math.floor(timeInSeconds / 14400);
+    timeInSeconds += rest * 3600;
+
+    return getSecondsInTime(timeInSeconds);
+  },
 };
-function getCarInfo(obj) {
-  for (let key in car) {
-    console.log(`${key}: ${obj[key]}`);
-  }
-}
-getCarInfo(car);
+console.log(car.getCarInfo());
+console.log(car.travelTime(320));
