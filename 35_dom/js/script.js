ac6874ac61040aefad3429a37cd239fe93b4203c
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-06-22 19:15:56 +0300

    tasks 35

--- 35_dom/js/script.js
+++ 35_dom/js/script.js
@@ -31,13 +31,29 @@ let filtered = [...firstDiv.children].filter(
 /*
 Создать функцию, которая принимает два элемента. Функция проверяет, является ли первый элемент родителем для второго:
 isParent(parent, child);
-isParent(document.body.children[0], document.querySelector('mark'));
-// true так как первый див является родительским элементом для mark
 isParent(document.querySelector('ul'), document.querySelector('mark'));
 // false так как ul НЕ является родительским элементом для mark
 Функция принимает только DOM объекты. Обязательно проверяйте это.
 */
 
-function isParent(parent, child) {}
+function isParent(parent, child) {
+  if (parent instanceof Element && child instanceof Element) {
+    return parent.contains(child);
+  } else {
+    return 'not DOM element';
+  }
+}
 
-// isParent(document.body.children[0], document.querySelector("mark"));
+// ================= 3 ==================
+// Получить список всех ссылок, которые не находятся внутри списка ul.
+const allLink = document.getElementsByTagName('a');
+Array.from(allLink).forEach(item => {
+  if (!item.closest('ul')) {
+    console.log(item);
+  }
+});
+
+// ================= 4 =================
+// Найти элемент, который находится перед и после списка ul
+console.log(document.querySelector('ul').nextElementSibling);
+console.log(document.querySelector('ul').previousElementSibling);
--- 36_elementsAttributes/js/script.js
+++ 36_elementsAttributes/js/script.js
@@ -8,10 +8,35 @@ const clos = link.closest('.content'); // Возвращает ближуйше�
 
 // div.classList.add('article', 'custom');
 // div.classList.contains('custom');
-const linkHref = link.href;
+// const linkHref = link.href;
 
-div.setAttribute('id', 'myId');
+// div.setAttribute('id', 'myId');
 
 // div.dataset;
 // div.dataset.myattr;
 // div.getAttribute('data-myattr');
+
+// DOM MANIPULATION
+const title = document.querySelector('h1');
+// title.innerHTML = '<span>text</span>';
+// title.innerHTML += '<span>new text</span>';
+
+// CREATE ELEMENTS
+const span = document.createElement('span');
+span.textContent = ' span created by createElement';
+span.classList.add('myClass');
+// title.appendChild(span);
+
+const fragment = document.createDocumentFragment(); // Дом узел
+const colors = ['black', 'yellow', 'orange'];
+colors.forEach(color => {
+  const item = document.createElement('div');
+  item.classList.add(`bg-${color}`);
+  item.style.background = color;
+  item.textContent = color;
+  fragment.appendChild(item);
+});
+document.body.appendChild(fragment);
+
+// title.remove();
+// title.parentElement.removeChild(title);
