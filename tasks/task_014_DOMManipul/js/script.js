02584e65fedbc80e0bc2f3d57b971625ccdc02ec
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-06-24 19:39:34 +0300

    added task folder + 014-1

---
+++ tasks/task_014_DOMManipul/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// ============= 1 ==============
+// Найти в коде список ul и добавить класс “list”
+const ul = document.querySelector('ul');
+
+ul.classList.add('list');
+
+// ============= 2 ==============
+// Найти в коде ссылку, находящуюся после списка ul, и добавить id=link
+const nextElement = nextElementSibling;
+nextElementSibling;
