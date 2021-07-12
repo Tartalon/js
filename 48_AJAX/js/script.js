2425aa316dafd8e7bb986186194dcdf4821edc74
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-07-12 23:04:30 +0300

    ajax_1 task renderUsers func

--- 48_AJAX/js/script.js
+++ 48_AJAX/js/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const btn = document.querySelector('button');
+const out = document.querySelector('.container-out');
+
 function getPosts(cb) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
@@ -15,6 +18,27 @@ function getPosts(cb) {
   xhr.send();
 }
 
-getPosts(responce => {
-  console.log(responce);
+function renderPosts(responce) {
+  const fragment = document.createDocumentFragment();
+  responce.forEach((post) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = post.title;
+    const article = document.createElement('p');
+    article.classList.add('card-text');
+    article.textContent = post.body;
+    cardBody.appendChild(title);
+    cardBody.appendChild(article);
+    card.appendChild(cardBody);
+    fragment.appendChild(card);
+  });
+  out.appendChild(fragment);
+}
+
+btn.addEventListener('click', (e) => {
+  getPosts(renderPosts);
 });
--- tasks/task_020_AJAX/js/script.js
+++ tasks/task_020_AJAX/js/script.js
@@ -1,3 +1,54 @@
 'use strict';
 
+// Получить пользователей (users) от сервера https://jsonplaceholder.typicode.com. Получив ответ от сервера вывести имена пользователей на страницу. При клике на имя пользователя в произвольном месте должна появиться подробная информация о нем. Для визуальной части можно использовать bootstrap или другие фреймворки.
+
 const list = document.querySelector('.list');
+const btn = document.querySelector('.btn');
+
+function getUsers(cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.addEventListener('load', () => {
+    const responce = JSON.parse(xhr.responseText);
+    cb(responce);
+  });
+  xhr.addEventListener('error', (e) => {
+    console.log('Error');
+  });
+
+  xhr.send();
+}
+
+function renderUsers(responce) {
+  const fragment = document.createDocumentFragment();
+  responce.forEach((user) => {
+    const li = document.createElement('li');
+    li.classList.add('list__item');
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const cardBody = document.createElement('div');
+    card.classList.add('card-body');
+    const title = document.createElement('h5');
+    card.classList.add('card-title');
+    title.textContent = user.name;
+    const text = document.createElement('p');
+    text.classList.add('card-text');
+    text.textContent = `
+      Address: street - ${user.address.street}, city - ${user.address.city}.
+      Company: name - ${user.company.name}.
+      Username - ${user.username}.
+      Phone - ${user.phone}
+    `;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(text);
+    card.appendChild(cardBody);
+    li.appendChild(card);
+    fragment.appendChild(li);
+  });
+  list.appendChild(fragment);
+}
+
+btn.addEventListener('click', (e) => {
+  getUsers(renderUsers);
+});
