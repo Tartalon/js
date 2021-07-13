c027b5c43cea3b28027b223e60c98071d6128ac0
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-07-13 18:25:31 +0300

    task 20 start

--- tasks/task_020_AJAX/js/script.js
+++ tasks/task_020_AJAX/js/script.js
@@ -2,53 +2,22 @@
 
 // Получить пользователей (users) от сервера https://jsonplaceholder.typicode.com. Получив ответ от сервера вывести имена пользователей на страницу. При клике на имя пользователя в произвольном месте должна появиться подробная информация о нем. Для визуальной части можно использовать bootstrap или другие фреймворки.
 
-const list = document.querySelector('.list');
-const btn = document.querySelector('.btn');
+// 1. Запрос получения пользователей
+// 2. Реализовать обработчик ответа от сервера
+// 3. Рендер списка пользователей
 
-function getUsers(cb) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
-  xhr.addEventListener('load', () => {
-    const responce = JSON.parse(xhr.responseText);
-    cb(responce);
-  });
-  xhr.addEventListener('error', (e) => {
-    console.log('Error');
+const apiURL = 'https://jsonplaceholder.typicode.com';
+
+function getUsersHTTP(cb) {
+  xhr = new XMLHttpRequest();
+
+  xhr.open('GET', `${apiURL}/users`);
+
+  xhr.addEventListener('onload', () => {
+    console.log(xhr.responseText);
   });
 
   xhr.send();
 }
 
-function renderUsers(responce) {
-  const fragment = document.createDocumentFragment();
-  responce.forEach((user) => {
-    const li = document.createElement('li');
-    li.classList.add('list__item');
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const cardBody = document.createElement('div');
-    card.classList.add('card-body');
-    const title = document.createElement('h5');
-    card.classList.add('card-title');
-    title.textContent = user.name;
-    const text = document.createElement('p');
-    text.classList.add('card-text');
-    text.textContent = `
-      Address: street - ${user.address.street}, city - ${user.address.city}.
-      Company: name - ${user.company.name}.
-      Username - ${user.username}.
-      Phone - ${user.phone}
-    `;
-
-    cardBody.appendChild(title);
-    cardBody.appendChild(text);
-    card.appendChild(cardBody);
-    li.appendChild(card);
-    fragment.appendChild(li);
-  });
-  list.appendChild(fragment);
-}
-
-btn.addEventListener('click', (e) => {
-  getUsers(renderUsers);
-});
+getUsersHTTP();
