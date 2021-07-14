4f742c4345171d60d556751d7897835c8a6056bc
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-07-14 18:51:05 +0300

    func render + func listItem

--- tasks/task_020_AJAX/js/script.js
+++ tasks/task_020_AJAX/js/script.js
@@ -7,17 +7,48 @@
 // 3. Рендер списка пользователей
 
 const apiURL = 'https://jsonplaceholder.typicode.com';
+const usersListEl = document.querySelector('.users-list');
 
 function getUsersHTTP(cb) {
-  xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
   xhr.open('GET', `${apiURL}/users`);
 
-  xhr.addEventListener('onload', () => {
-    console.log(xhr.responseText);
+  xhr.addEventListener('load', () => {
+    if (xhr.status !== 200) {
+      console.log('Error', xhr.status);
+      return;
+    }
+
+    const res = JSON.parse(xhr.responseText);
+
+    cb(res);
   });
 
   xhr.send();
 }
 
-getUsersHTTP();
+function onGetUsersCallback(users) {
+  if (!users.length) {
+    return;
+  }
+
+  renderUsersList(users);
+}
+
+function renderUsersList(users) {
+  const fragment = users.reduce(
+    (acc, user) => acc + userListItemTemplate(user),
+    ''
+  );
+}
+
+function userListItemTemplate(user) {
+  return `
+    <button type="button" class="list-group-item list-group-item-action">
+      ${user.name}
+    </button>
+  `;
+}
+
+getUsersHTTP(onGetUsersCallback);
