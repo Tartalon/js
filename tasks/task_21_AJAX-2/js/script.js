5aed56a113e6cd6a7935acd314e34f7470c88b4f
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-07-19 18:41:30 +0300

    function getUsers

--- tasks/task_21_AJAX-2/js/script.js
+++ tasks/task_21_AJAX-2/js/script.js
@@ -7,13 +7,21 @@ const inputName = document.querySelector('.input-name');
 const inputEmail = document.querySelector('.input-email');
 const inputTel = document.querySelector('.input-tel');
 const inpuWebsite = document.querySelector('.inpu-website');
+const out = document.querySelector('.users-list');
 const apiURL = 'https://jsonplaceholder.typicode.com';
 
-function getUsers() {
+function getUsers(cb) {
   const xhr = XMLHttpRequest();
   xhr.open('POST', `${apiURL}/users`);
 
   xhr.addEventListener('load', () => {
     const response = JSON.parse(xhr.responseText);
+    cb(response);
+  });
+
+  xhr.addEventListener('error', () => {
+    console.log('Error');
   });
 }
+
+function renderUsers(response) {}
