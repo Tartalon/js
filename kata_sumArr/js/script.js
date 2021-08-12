a901a413d1cb88a8330b86535407d275a8c888d1
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-08-12 16:54:15 +0300

    arr in arr multiply

--- kata_sumArr/js/script.js
+++ kata_sumArr/js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 // Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 // Examples
 
@@ -10,8 +11,7 @@
 // Input: [-2.398]
 // Output: -2.398
 
-function sum (numbers) {
-  "use strict";
+function sum(numbers) {
   let summ = 0;
   if (numbers.length === 0) {
     summ = 0;
@@ -23,14 +23,36 @@ function sum (numbers) {
     }
   }
   return summ;
-};
-console.log(sum([4, 15, 4]));
-
+}
+// console.log(sum([4, 15, 4]));
 
 // sum = function (numbers) {
-//   "use strict";
 //   return numbers.reduce(function(t, n){
 //     return t + n;
 //   }, 0);
 // };
 
+// ===========================================
+// 1
+
+let input = [[1, 5], [2], [-1, -3]];
+
+function smallestProduct(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(
+      arr[i].reduce(function (acc, current) {
+        return acc * current;
+      })
+    );
+  }
+  let res = newArr[0];
+  for (let elem of newArr) {
+    res = elem < res ? elem : res;
+  }
+  return res;
+}
+
+// 2
+const smallestProduct = a => Math.min(...a.map(e => e.reduce((p, c) => p * c)));
