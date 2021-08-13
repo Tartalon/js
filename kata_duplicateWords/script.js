058846f404ad7d465cf95f4fca36ac534b611792
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-08-13 15:22:56 +0300

    removeDuplicate func

--- kata_duplicateWords/script.js
+++ kata_duplicateWords/script.js
@@ -1,10 +1,22 @@
 'use strict';
 // Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
 function removeDuplicateWords(s) {
-  let arrayFromS = s.split(' ');
-  let newStr = arrayFromS.map(word => );
+  // let newStr = [];
+
+  // for (const word of arrayFromS) {
+  //   if (!newStr.includes(word)) {
+  //     newStr.push(word);
+  //   }
+  // }
+  // return newStr;
+
+  // let arrayFromS = s.split(' ');
+  return Array.from(new Set(s.split(' '))).join(' ');
 }
 
-removeDuplicateWords(
-  'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+// const removeDuplicateWords2 = s => [...new Set(s.split(' ')).join(' ')];
+console.log(
+  removeDuplicateWords(
+    'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+  )
 );
