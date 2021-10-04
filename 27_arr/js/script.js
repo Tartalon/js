91cb79aceb70f5f6082215e2c88e6aac37af4a1a
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-10-04 16:33:55 +0300

    function createLi

--- 27_arr/js/script.js
+++ 27_arr/js/script.js
@@ -6,117 +6,56 @@
 // Добавление покупки в список. Учтите, что при добавлении покупки с уже существующим в списке продуктом, необходимо увеличивать количество в существующей покупке, а не добавлять новую.
 // Покупка продукта. Функция принимает название продукта и отмечает его как купленный
 
-const productsAll = document.querySelector('.products__all');
-const productsBuy = document.querySelector('.products__buy');
-const productsList = document.querySelector('.products__list');
-const formAddingInput = document.querySelector('.form-adding__input');
-const formAddingButton = document.querySelector('.form-adding__button');
-const productBuy = document.querySelector('.product-buy');
-
-let shoppingList = [
+let products = [
   {
-    productsName: 'apple',
-    quantity: 1,
-    bought: false,
-  },
-  {
-    productsName: 'pear',
-    quantity: 1,
-    bought: false,
-  },
-  {
-    productsName: 'carrot',
-    quantity: 1,
+    name: 'apple',
+    quantity: 3,
     bought: true,
   },
   {
-    productsName: 'melon',
+    name: 'banana',
+    quantity: 4,
+    bought: false,
+  },
+  {
+    name: 'bread',
+    quantity: 1,
+    bought: false,
+  },
+  {
+    name: 'meat',
     quantity: 1,
     bought: false,
   },
 ];
 
-productsAll.addEventListener('click', e => {
-  e.preventDefault();
+const productInput = document.querySelector('.product__input');
+const productAddBtn = document.querySelector('.product__add');
+const productsList = document.querySelector('.products__list');
+const productsAllBtn = document.querySelector('.products__all');
 
-  sortByBought();
-  showProducts();
-  bought();
+function createLi(product) {
+  let li = document.createElement('li');
+  li.classList.add('products__item');
+
+  let p = document.createElement('p');
+  p.classList.add('product__name');
+  p.textContent = product.name;
+
+  let span = document.createElement('span');
+  span.classList.add('product__quantity');
+  span.textContent = product.quantity;
+
+  let checkbox = document.createElement('checkbox');
+  checkbox.classList.add('product__check');
+
+  let buyBtn = document.createElement('button');
+  buyBtn.classList.add('product__buy');
+
+  li.append(p, span, checkbox, buyBtn);
+  return li;
+}
+
+productsAllBtn.addEventListener('click', function () {
+  products.forEach(product => productsList.append(createLi(product)));
 });
-
-formAddingButton.addEventListener('click', e => {
-  e.preventDefault();
-
-  addProduct();
-  sortByBought();
-});
-
-function showProducts() {
-  productsList.textContent = '';
-
-  for (let i = 0; i < shoppingList.length; i++) {
-    const product = shoppingList[i];
-    const bought = product.bought === true ? 'Yes' : 'No';
-    const quantity = 0;
-
-    productsList.insertAdjacentHTML(
-      'beforeend',
-      `<li class="product__list-inner">
-          <span class="product__title product__span">${product.productsName}</span>
-          <span class="product__quantity product__span">${product.quantity}</span>
-          <div class="product__bought product__span">
-          <span class="bought-span">${bought}</span>
-          <button class="product-buy products-button">Buy</button>
-        </div>
-      </li>`
-    );
-  }
-}
-
-function addProduct() {
-  const inputValue = formAddingInput.value;
-  let newProduct;
-
-  if (inputValue === '') {
-    return;
-  } else {
-    newProduct = {
-      productsName: inputValue,
-      quantity: 1,
-      bought: false,
-    };
-  }
-
-  shoppingList.push(newProduct);
-  const bought = newProduct.bought === true ? 'Yes' : 'No';
-
-  sortByBought();
-
-  productsList.insertAdjacentHTML(
-    'afterbegin',
-    `<li class="product__list-inner">
-          <span class="product__title product__span">${newProduct.productsName}</span>
-          <span class="product__quantity product__span">${newProduct.quantity}</span>
-          <div class="product__bought product__span">
-          <span class="bought-span">${bought}</span>
-          <button class="product-buy products-button">Buy</button>
-        </div>
-      </li>`
-  );
-
-  clearInput();
-}
-
-function sortByBought() {
-  return shoppingList.sort((a, b) => (a.bought > b.bought ? 1 : -1));
-}
-
-function clearInput() {
-  formAddingInput.value = '';
-}
-
-function bought() {
-  productBuy.addEventListener('click', e => {
-    console.log(e.target);
-  });
-}
